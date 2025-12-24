7ad1288b2687d539fef2b8a80ba3ae6c2d470a8e
helga <olga.a.karpova@gmail.com>
2025-12-24 18:14:51 +0300

    ansible

--- srv_ansible/start.sh
+++ srv_ansible/start.sh
@@ -46,7 +46,7 @@ chmod 0440 "${SUDOERS_FILE}"
 
 
 #разворачиваем kesl
-cat >/home/admin/kesl.ini <<EOF
+cat >/home/ansible/kesl.ini <<EOF
 EULA_AGREED=yes
 PRIVACY_POLICY_AGREED=yes
 SERVICE_LOCALE=en_US.UTF-8
@@ -57,15 +57,12 @@ GROUP_CLEAN=no
 ScanMemoryLimit=2048
 USE_SYSTEMD=yes
 EOF
-chown admin:admin /home/admin/kesl.ini
+chown ansible:ansible /home/ansible/kesl.ini
 
-# Разворачиваем sshd + adm пользователя
-chmod +x /usr/local/bin/adm.sh
-/usr/local/bin/adm.sh
-/usr/sbin/sshd
 
 sudo apt-get install /tmp/kesl_12.3.0-1162_amd64.deb
 sudo /opt/kaspersky/kesl/bin/kesl-setup.pl --autoinstall=/home/admin/kesl.ini
+
 
 # Keep container alive
 tail -f /dev/null
